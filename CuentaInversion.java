22438c343c2f51d1e550803f7f79761473ad7f85
Ulises Gachuz <109196509+UGachuzD@users.noreply.github.com>
2023-05-01 15:33:37 -0600

    Add files via upload
    Parte verde hecha

---
+++ CuentaInversion.java
@@ -0,0 +1,21 @@
+/**
+ * Clase CuentaInverison 
+ */
+public class CuentaInversion {
+    // Atributos
+    private String id;
+    private double balance;
+    private double tarifa;
+    private String nombre;
+
+    // Metodos
+    // Metodo Constructor
+    CuentaInversion(String id, double balance, double tarifa, String nombre){
+        this.id = id;
+        this.balance = balance;
+        this.tarifa = tarifa;
+        this.nombre = nombre;
+    }
+}
+
+
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Practica 3");
+        CuentaInversion obj1 = new CuentaInversion("1", 20, 2, "Ulises");
+    }
+}
